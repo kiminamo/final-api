3c0daf87bea8aed1ab903f70c71725ee11dc762e
kiminamo <manalotokimm@gmail.com>
2025-05-15 15:19:21 +0800

    first commit

---
+++ src/main/java/com/manaloto/finalprojectitel3/FinalProjectITel3Application.java
@@ -0,0 +1,13 @@
+package com.manaloto.finalprojectitel3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectITel3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectITel3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/manaloto/finalprojectitel3/FinalProjectITel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manaloto.finalprojectitel3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectITel3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
